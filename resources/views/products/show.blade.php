0b013c1dbb374f96237aedb946b34bf77cdf791c
PerifeN <sergiuszs7@wp.pl>
2024-12-24 10:36:04 +0100

    added "add to cart" button in product view

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -14,9 +14,28 @@
                             <div class="d-flex justify-content-center align-items-center" style="width: 300px; height: 300px; background-color: #6c757d; color: #fff; border-radius: 5px;">
                                 <p class="text-center">300x300 Placeholder</p>
                             </div>
-
+                            
+                           
                         <p class="card-text mt-3"><strong>Description:</strong> {{ $product->desc }}</p>
-                        <a href="{{ route('products.index') }}" class="btn btn-secondary">Back to Products</a>
+
+                        
+                        <div class="btn-group mr-2">
+                            <a href="{{ route('products.index') }}">
+                                <button class="btn btn-secondary">Back to Products</button>  <!-- przejscie do produktów -->
+                            </a>
+                        </div>
+                        <div class="btn-group mr-2">    
+                                @auth
+                                    <form method="POST" action="{{ route('products.addToCart', $product->id) }}">
+                                    @csrf
+                                        <button type="submit" class="btn btn-success">Add to cart</button> <!-- dodanie to basket -->
+                                    </form>
+                                @else
+                                    <p><a href="{{ route('login') }}">Sign in</a>, to add item to cart.</p>
+                                @endauth
+
+                        </div> 
+
                     </div>
                 </div>
             </div>
